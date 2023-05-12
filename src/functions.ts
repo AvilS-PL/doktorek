d099b89a7b0b5c8e211feeca0944faa2d6d3c417
mateusz <mateuszeksli@o2.pl>
2023-05-13 01:12:13 +0200

    v0.4.2.6 still unstable, need to go back before canvas

--- src/functions.ts
+++ src/functions.ts
@@ -22,14 +22,13 @@ export function imgLoad() {
 
 }
 
+let canvas = document.createElement("canvas")
+let ctx = canvas.getContext("2d")
 export function renderAny(x: number, y: number, w: number, h: number, rx: number, ry: number, rw: number, rh: number, what: string) {
-
     // console.log("render")
     let el = document.getElementById(what)
-    let canvas = document.createElement("canvas")
     canvas.width = rw
     canvas.height = rh
-    let ctx = canvas.getContext("2d")
     ctx.drawImage(img, x, y, w, h, rx, ry, rw, rh)
     let url = canvas.toDataURL()
     el.style.width = rw + "px"
